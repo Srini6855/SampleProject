387fb3983ab37c1c77ee89e925fa621d327dfa46
Srini6855 <srinipmps@gmail.com>
2024-08-11 16:06:51 +0530

    this is first commit

---
+++ src/test/java/org/booking/Booking.java
@@ -0,0 +1,19 @@
+package org.booking;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Booking {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://omrbranch.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.id("email")).sendKeys("srinipmps@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("8@ndHEw5TfaLm5e");
+	}
+
+}
